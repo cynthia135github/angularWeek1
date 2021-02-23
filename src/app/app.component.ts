5eaa9678b36ba543a22564f7d598f04078620006
cynthia135github <c14180135@john.petra.ac.id>
2021-02-23 16:57:52 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  hasil = 0;
+  a = 0;
+  b = 0;
+
+  item = [
+    {nama : "Jeruk", harga : 15000, satuan : "kg"},
+    {nama : "Strawberry", harga : 12000, satuan : "kg"}
+  ]
+
+  tampil : boolean = true;
+  tulisanOpen : String;
+
+  OPEN(){
+    this.tulisanOpen="TOKO DIBUKA";
+    alert("DIBUKA");
+  }
+
+  values = "";
+  onKey(event: any){
+    this.values += event.target.value + '|';
+  }
+
 }
